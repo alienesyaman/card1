3877645d851f4de61e262884980535ff66f2a75f
Ali Enes Yaman <92275400+alienesyaman@users.noreply.github.com>
2023-10-04 15:25:04 +0300

    card

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Grid from "./components/Grid"
+
+function App() {
+  return (
+    <div >
+<Grid></Grid>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Grid.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import "./Grid.css"
+import img1 from "../image/unnamed.png"
+
+
+function Grid() {
+  return (
+    <div>
+
+    <div class="grid-container">
+
+  <div className='grid-1'>
+    <img src={img1}></img>
+    
+     <div>
+    <p>Stefan Kuntz'a ödenecek
+tazminat belli oldu</p>
+<div className='line'></div>
+<div className='bottom-top'><p>Haber Kaynağı</p><p>Paylaşım T.</p> </div>
+<div className='bottom-bottom'><p>R.Pehlivanlar</p><p>29 Aralık 2023</p> </div>
+</div>
+  </div>
+
+  <div className='grid-1'>
+  <img src={img1}></img>
+    <p>Stefan Kuntz'a ödenecek
+tazminat belli oldu</p>
+<div className='line'></div>
+<div className='bottom-top'><p>Haber Kaynağı</p><p>Paylaşım T.</p> </div>
+<div className='bottom-bottom'><p>R.Pehlivanlar</p><p>29 Aralık 2023</p> </div>
+  </div>
+
+  <div className='grid-1'>
+  <img src={img1}></img>
+    <p>Stefan Kuntz'a ödenecek
+tazminat belli oldu</p>
+<div className='line'></div>
+<div className='bottom-top'><p>Haber Kaynağı</p><p>Paylaşım T.</p> </div>
+<div className='bottom-bottom'><p>R.Pehlivanlar</p><p>29 Aralık 2023</p> </div>
+    </div>  
+
+  <div className='grid-1'>
+  <img src={img1}></img>
+    <p>Stefan Kuntz'a ödenecek
+tazminat belli oldu</p>
+<div className='line'></div>
+<div className='bottom-top'><p>Haber Kaynağı</p><p>Paylaşım T.</p> </div>
+<div className='bottom-bottom'><p>R.Pehlivanlar</p><p>29 Aralık 2023</p> </div>
+  </div>
+
+  <div className='grid-1'>
+  <img src={img1}></img>
+    <p>Stefan Kuntz'a ödenecek
+tazminat belli oldu</p>
+<div className='line'></div>
+<div className='bottom-top'><p>Haber Kaynağı</p><p>Paylaşım T.</p> </div>
+<div className='bottom-bottom'><p>R.Pehlivanlar</p><p>29 Aralık 2023</p> </div>
+  </div>
+
+  <div className='grid-1'>
+  <img src={img1}></img>
+    <p>Stefan Kuntz'a ödenecek
+tazminat belli oldu</p>
+<div className='line'></div>
+<div className='bottom-top'><p>Haber Kaynağı</p><p>Paylaşım T.</p> </div>
+<div className='bottom-bottom'><p>R.Pehlivanlar</p><p>29 Aralık 2023</p> </div>
+    </div>  
+
+  <div className='grid-1'>
+  <img src={img1}></img>
+    <p>Stefan Kuntz'a ödenecek
+tazminat belli oldu</p>
+<div className='line'></div>
+<div className='bottom-top'><p>Haber Kaynağı</p><p>Paylaşım T.</p> </div>
+<div className='bottom-bottom'><p>R.Pehlivanlar</p><p>29 Aralık 2023</p> </div>
+  </div>
+
+  <div className='grid-1'>
+  <img src={img1}></img>
+    <p>Stefan Kuntz'a ödenecek
+tazminat belli oldu</p>
+<div className='line'></div>
+<div className='bottom-top'><p>Haber Kaynağı</p><p>Paylaşım T.</p> </div>
+<div className='bottom-bottom'><p>R.Pehlivanlar</p><p>29 Aralık 2023</p> </div>
+    </div>  
+
+
+</div>  
+
+    </div>
+  )
+}
+
+export default Grid
